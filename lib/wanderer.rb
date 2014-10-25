088f5d59847ee72eafe4a913d8d1aebfa6a54dbe
yunlei <yunlei.liu@appfolio.com>
2014-10-25 00:02:16 -0700

    use TracePoint to subscript call event

--- lib/wanderer.rb
+++ lib/wanderer.rb
@@ -1,5 +1,18 @@
 require "wanderer/version"
 
 module Wanderer
-  # Your code goes here...
+  def self.walk
+    foot_print = []
+
+    trace = TracePoint.new(:call) do |tp|
+      foot_print << "=> #{tp.defined_class} #{tp.inspect}"
+    end
+
+    trace.enable
+    yield
+
+  ensure
+    trace.disable
+    return foot_print
+  end
 end
