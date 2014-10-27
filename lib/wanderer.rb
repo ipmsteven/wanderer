4d539cea7461271aa65abe59fda27dfa065b584c
yunlei <yunlei.liu@appfolio.com>
2014-10-26 22:05:53 -0700

    follow Ruby inheritance hierarchy to look up method

--- lib/wanderer.rb
+++ lib/wanderer.rb
@@ -1,18 +1,38 @@
 require "wanderer/version"
+require 'graphviz'
 
 module Wanderer
-  def self.walk
-    foot_print = []
 
-    trace = TracePoint.new(:call) do |tp|
-      foot_print << "=> #{tp.defined_class} #{tp.inspect}"
+  def self.walk(receiver, look_up_method)
+    visited = search(receiver, look_up_method)
+
+    g = GraphViz.new( :G, :type => :digraph )
+
+    visited[0...-1].each_with_index do |item, index|
+      a = g.add_nodes(item.to_s)
+      b = g.add_nodes(visited[index+1].to_s)
+      g.add_edges(a, b)
     end
 
-    trace.enable
-    yield
+    # Generate output image
+    g.output( :png => "chain.png" )
+    visited
+  end
 
-  ensure
-    trace.disable
-    return foot_print
+  def self.search(receiver, look_up_method)
+    visited = []
+    ancestors = receiver.singleton_class.ancestors
+
+    ancestors.each do |ancestor|
+      visited << ancestor
+      return visited if ancestor.instance_methods(false).include?(look_up_method.to_sym)
+    end
+
+    visited << "method_missing"
+
+    ancestors.each do |ancestor|
+      visited << ancestor
+      return visited if ancestor.instance_methods(false).include? :method_missing
+    end
   end
 end
